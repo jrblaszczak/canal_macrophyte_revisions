5c688de57d538697c49ae1fa59f941fd843831c8
Joanna Blaszczak <joanna.r.blaszczak@gmail.com>
2026-05-07 16:36:51 -0700

    Updated travel time calculations

--- code and data/10_Brehob_Revisions_TravelTimeUpdates.R
+++ code and data/10_Brehob_Revisions_TravelTimeUpdates.R
@@ -23,12 +23,21 @@ tt$Canal <- revalue(tt$ID, replace = c("AMetabModelingFlow.csv" = "A",
                                        "DRLMetabModelingFlow.csv" = "UL",
                                        "MSLMetabModelingFlow.csv" = "DL"))
 
-## Remove old travel time calculation
+## First, extract DO sensor reach length
+## Second, remove old travel time calculation
 
+# (1) Back calculate the length
+tt$Length <- tt$Velocity*tt$TravelTime
+# (2) TT was in days - multiply by the 86,400 seconds/day to get m
+tt$Length <- tt$Length*86400
+# (3) Create a travel time in hours
+tt$TravelTime_seconds <- tt$Length/tt$Velocity
+tt$TravelTime_hours <- tt$TravelTime_seconds/3600
 
+## Calculate the mean travel time per canal
 
-
-
-
+tt %>%
+  group_by(Canal) %>%
+  summarise_at(.vars = 'TravelTime_hours', .funs = mean)
 
 
